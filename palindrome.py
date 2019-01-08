4b63bc8953a80bbae13df254c2dc2eb765b67aaf
AishwaryaKaminiRajendran <46441583+AishwaryaKaminiRajendran@users.noreply.github.com>
2019-01-08 16:56:42 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,16 +1,7 @@
 N=int(input())
 t=N
-while N<=1000:
-	d=N%10
-	r=r*10+d
-	N=N//10
-if (r==t):
-	print("yes")
-else:
-	print("no")
-N=int(input())
-t=N
-while N<=1000:
+r=0
+while N>0:
 	d=N%10
 	r=r*10+d
 	N=N//10
