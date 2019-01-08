caea7489c2df69bf6e98e4b8ae4bfddf89d6cc72
AishwaryaKaminiRajendran <46441583+AishwaryaKaminiRajendran@users.noreply.github.com>
2019-01-08 15:37:13 +0530

    Create alphabet.py

---
+++ alphabet.py
@@ -0,0 +1,7 @@
+x=input()
+if x=='a' and x=='e' and x=='i' and x=='o' and x=='u' and x=='A' and x=='E' and x=='I' and x=='O' and x=='U':
+	print("Vowel")
+elif x!='a' and x!='e' and x!='i' and x!='o' and x!='u' and x!='A' and x!='E' and x!='I' and x!='O' and x!='U':
+	print("Consonant")
+else:
+	print("invalid")
