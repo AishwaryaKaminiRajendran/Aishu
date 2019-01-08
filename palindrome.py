a1fba26e657e5f7b52e5411dc38fb146972a3c94
AishwaryaKaminiRajendran <46441583+AishwaryaKaminiRajendran@users.noreply.github.com>
2019-01-08 16:47:19 +0530

    Create palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,20 @@
+N=int(input())
+t=N
+while N<=1000:
+	d=N%10
+	r=r*10+d
+	N=N//10
+if (r==t):
+	print("yes")
+else:
+	print("no")
+N=int(input())
+t=N
+while N<=1000:
+	d=N%10
+	r=r*10+d
+	N=N//10
+if (r==t):
+	print("yes")
+else:
+	print("no")
